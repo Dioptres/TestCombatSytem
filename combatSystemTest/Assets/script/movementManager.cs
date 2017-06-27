b00302a363ae820c37102cd1cbd37b2ed4ba28a2
Dioptre <misiak.benjamin@gmail.com>
2017-06-27 15:18:32 +0200

    things

---
+++ combatSystemTest/Assets/script/movementManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movementManager : MonoBehaviour {
+
+	static Vector3 _directionDash = Vector3.zero;
+
+	public static Vector3 directionMove;
+	
+	public static Vector3 directionDash
+	{
+		get { return _directionDash; }
+		set { _directionDash += value; }
+	}
+
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		this.transform.position = this.transform.position + directionMove + directionDash;
+
+		directionMove = Vector3.zero;
+	}
+}
--- combatSystemTest/Assets/script/shoot.cs
+++ combatSystemTest/Assets/script/shoot.cs
@@ -4,11 +4,18 @@ using UnityEngine;
 
 public class shoot : MonoBehaviour {
 
+	public AudioClip shootSound;
+	public AudioClip superShootSound;
+
+	AudioSource source;
+
 	Vector3 mousePos;
 	Vector3 line;
 	Vector3 graphLine;
 	Vector3 lineForDraw;
 	private LineRenderer _lineRenderer;
+
+	float timerShoot;
 
 	Vector3 move;
 
@@ -17,13 +24,17 @@ public class shoot : MonoBehaviour {
 	// Use this for initialization
 	void Start ()
 	{
+		source = GetComponent<AudioSource> ();
+
+		timerShoot = 0;
+
 		_lineRenderer = gameObject.AddComponent<LineRenderer> ();
 		_lineRenderer.startWidth = 0.05f;
 		move = Vector3.zero;
 	}
 	
 	// Update is called once per frame
-	void Update ()
+	void LateUpdate ()
 	{
 		this.transform.position += move;
 		if (move.magnitude > 1)
@@ -60,13 +71,36 @@ public class shoot : MonoBehaviour {
 		_lineRenderer.SetPosition (0, this.transform.position);
 		_lineRenderer.SetPosition (1, graphLine);
 
-		if(Input.GetMouseButtonDown (0))
+		if(Input.GetMouseButton (0))
 		{
+			Debug.Log (timerShoot);
+			timerShoot += Time.deltaTime;
+			if(timerShoot > 2) { timerShoot = 2; }
 			//move -= line/2;
+			//source.PlayOneShot (shootSound);
+
 		}
+		else if (Input.GetMouseButtonUp (0))
+		{
+			Debug.Log (timerShoot);
+			if(timerShoot < 1)
+			{
+				source.PlayOneShot (shootSound);
+			}
+			else
+			{
+				source.PlayOneShot (superShootSound);
+			}
+			movementManager.directionDash = -line*(timerShoot*timerShoot + 1);
+			timerShoot = 0;
+		}
+
+
 		if (Input.GetMouseButtonDown (1))
 		{
-			move -= line/2;
+			//move -= line/2;
+			//source.PlayOneShot (superShootSound);
+			movementManager.directionDash = line;
 		}
 
 
